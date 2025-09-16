3971c39ac0c9c1613091d4fc1021f1b6f15417f2
retroblaz <153559220+pranavdixiit@users.noreply.github.com>
2025-09-16 14:43:38 +0530

    Initial commit

---
+++ estate-frontend/src/index.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { Provider, useDispatch } from 'react-redux';
+import store from './store';
+import { fetchUser } from './features/auth/authSlice';
+import App from './App';
+import { HashRouter } from 'react-router-dom';
+
+const AuthLoader = ({ children }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      dispatch(fetchUser());
+    }
+  }, [dispatch]);
+
+  return children;
+};
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+
+root.render(
+  <Provider store={store}>
+    <AuthLoader>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </AuthLoader>
+  </Provider>
+);
+
